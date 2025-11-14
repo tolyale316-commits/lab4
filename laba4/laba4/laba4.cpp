28473e1d876389e9b16ef7a4ab65c96e5822a7d7
tolyale316-commits <tolya.le316@gmail.com>
2025-11-14 11:01:22 +0400

    kommit1

---
+++ laba4/laba4/laba4.cpp
@@ -0,0 +1,20 @@
+﻿// laba4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <stdio.h>
+
+int main() {
+    int N;
+
+    printf("Введите число N: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = N; j > N - i; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
